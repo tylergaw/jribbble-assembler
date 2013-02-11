fb421a71b8670119421a5ea29460b7f0d653521d
Tyler Gaw <me@tylergaw.com>
2013-02-10 23:30:16 -0500

    Fixing compass conf, body background image

--- conf/compass/config.rb
+++ conf/compass/config.rb
@@ -1,21 +1,9 @@
 # This is a compass (sass/stylesheet) configuration file.
-#
-# Require any additional compass plugins here.
-# Set this to the root of your project when deployed:
-http_path = "/"
-
 css_dir = "../../css"
 sass_dir = "../../css/scss"
-
-# Images
 images_dir = "../../images"
-
-# Output
-output_style = :expanded
-line_comments = false
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
+output_style = :compressed
+line_comments = true
 relative_assets = true
 preferred_syntax = :scss
-
 asset_cache_buster :none
